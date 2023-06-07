5177b290942c4b8dfad700eeb303512289422811
Kasemsan <kasemsan.cho@gmail.com>
2023-06-07 15:26:17 +0700

    dev

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import data from "./fakeData/data.json" assert { type: "json" };
-import statistics from "./statistics.json" assert { type: "json" };
+import statistics from "./fakeData/statistics.json" assert { type: "json" };
 import agentList from "./fakeData/agentList.json" assert { type: "json" };
 import lineLanguage from "./fakeData/lineLanguage.json" assert { type: "json" };
 import centerBranch from "./fakeData/centerBranch.json" assert { type: "json" };
