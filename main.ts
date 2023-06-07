58961ef900036d258771456b214c88a4f74c6eb9
Kasemsan <kasemsan.cho@gmail.com>
2023-06-07 15:17:15 +0700

    dev

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
-import statistics from './statistics.json' assert { type: "json" };
-import agentList from './agentList.json' assert { type: "json" };
-import lineLanguage from './lineLanguage.json' assert { type: "json" };
-import centerBranch from './centerBranch.json' assert { type : "json"};
-import abandonList from './abandonList.json' assert { type : "json"}
+import data from "./fakeData/data.json" assert { type: "json" };
+import statistics from "./statistics.json" assert { type: "json" };
+import agentList from "./fakeData/agentList.json" assert { type: "json" };
+import lineLanguage from "./fakeData/lineLanguage.json" assert { type: "json" };
+import centerBranch from "./fakeData/centerBranch.json" assert { type: "json" };
+import abandonList from "./fakeData/abandonList.json" assert { type: "json" };
+import incomingSummaryCenter from "./fakeData/incomingSummaryCenter.json" assert { type: "json" };
+import incomingSummaryBranch from "./fakeData/incomingSummaryBranch.json" assert { type: "json" };
 
 const router = new Router();
 router
@@ -18,31 +20,30 @@ router
   .get("/12/status/24hr", (context) => {
     context.response.body = statistics;
   })
-  .get("/12/agent", (context) => {
+  .get("/agent/center", (context) => {
+    context.response.body = agentList;
+  })
+  .get("/agent/branch", (context) => {
     context.response.body = agentList;
   })
   .get("/12/line-language", (context) => {
-    context.response.body = lineLanguage
+    context.response.body = lineLanguage;
   })
   .get("/line/branch", (context) => {
-    context.response.body = centerBranch
+    context.response.body = centerBranch;
   })
-  .get('/12/abandon/list', (context) => {
-    context.response.body = abandonList
+  .get("/abandon/center", (context) => {
+    context.response.body = abandonList;
   })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
+  .get("/12/abandon/branch", (context) => {
+    context.response.body = abandonList;
+  })
+  .get("/incoming/summary/center", (context) => {
+    context.response.body = incomingSummaryCenter;
+  })
+  .get("/12/incoming/summary/branch", (context) => {
+    context.response.body = incomingSummaryBranch;
   });
-
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
